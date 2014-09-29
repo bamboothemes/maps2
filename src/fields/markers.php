b6ed3aa75bebd8f32f265f986ca3db82eb66a5fb
Robert Went <i@robertwent.com>
2014-09-29 19:15:17 +0100

    to avoid confusion over missing marker fields

--- src/fields/markers.php
+++ src/fields/markers.php
@@ -7,6 +7,6 @@ jimport('joomla.form.formfield');
 class JFormFieldMarkers extends JFormField {
 
 	public function getInput() {
-return '<div id="markers">This is where the marker fields will go</div>';
+return '<div id="markers">Marker fields will appear here after adding a marker by clicking on the map</div>';
 }
 }
